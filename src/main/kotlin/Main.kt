821d82515e8b80b52aec10aa9a874b71b101df89
Yunhong Min <yunhongmin00@gmail.com>
2026-06-03 10:25:01 +0900

    practice: simple

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,9 +8,17 @@ fun main() {
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
 
+    var maxValue = 0
+
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
+        maxValue = max(maxValue, i)
     }
+
+    println("maxValue = $maxValue")
+    println("${maxValue}는 큰 수")
 }
+
+fun max(a: Int, b: Int): Int =  if (a > b) a else b
